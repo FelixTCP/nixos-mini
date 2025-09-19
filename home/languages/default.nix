602b67b287b6919217a8285c69210144d7425579
Felix Wensky <felix.wensky@gmail.com>
2025-09-19 23:33:27 +0200

    fix syntax on language configs

--- home/languages/default.nix
+++ home/languages/default.nix
@@ -1,7 +1,5 @@
 # Import all language configurations
-{ ... }:
-
-{
+{ pkgs, ... }: {
   imports = [ ./lua.nix ./nix.nix ./python.nix ./angular.nix ./java.nix ];
 }
 
--- home/languages/java.nix
+++ home/languages/java.nix
@@ -1,4 +1,4 @@
-{ pkgs }: {
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     # Java SDK
     zulu
